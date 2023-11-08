8b38a1ed6585e387b1708f4cd83ec7865c67f1c1
omar.dhakouani <dhakouaniomar@gmail.com>
2023-11-08 14:39:28 +0100

    November 2023

---
+++ LED_Blink.py
@@ -0,0 +1,14 @@
+from gpiozero import LED, Button
+from time import sleep
+
+pushButton = Button(3)
+blinkenlight = LED(21)
+
+while True:
+    sleep(1)
+    if pushButton.is_pressed :
+        blinkenlight.on()
+        print("Button is pressed")
+    else:
+        blinkenlight.toggle()
+        print("button is NOT pressed")
